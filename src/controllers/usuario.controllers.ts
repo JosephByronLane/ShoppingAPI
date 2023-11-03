fbe0e5d90f4157392089fa1357b0ea612b813403
JosephByronLane <2419141@modelo.edu.mx>
2023-11-02 18:33:48 -0600

    added GET for users

--- src/controllers/usuario.controllers.ts
+++ src/controllers/usuario.controllers.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express"
 import { Usuario } from "../entities/Usuario"
+import { ResumeOptions } from "typeorm";
 
 export const createUser = async(req : Request,res : Response) => {
     try{
@@ -11,8 +12,16 @@ export const createUser = async(req : Request,res : Response) => {
         return res.json(usuario)
     }
     catch(error){
-        if(error instanceof Error){
-            return res.status(500).json({message: error.message})
-        }
+        if(error instanceof Error)  return res.status(500).json({message: error.message})
+    }
+}
+
+export const getUsers = async (req: Request, res: Response) =>{
+    try{
+        const users = await Usuario.find();
+        return res.json(users)
+    }
+    catch(error){
+        if(error instanceof Error) return res.status(500).json({message: error.message})
     }
 }
--- src/routes/Usuario.routes.ts
+++ src/routes/Usuario.routes.ts
@@ -1,10 +1,13 @@
 import {Router} from 'express'
 import { createUser } from '../controllers/usuario.controllers'
+import { getUsers } from '../controllers/usuario.controllers'
+
 import { create } from 'domain'
 
 
 const router = Router()
 
 router.post('/usuario',createUser)
+router.get('/usuario', getUsers)
 
 export default router
