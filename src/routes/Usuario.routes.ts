e0296cbb10fb13eaeac6b4f9810085809a633ada
JosephByronLane <2419141@modelo.edu.mx>
2023-11-09 19:08:30 -0600

    honestly im just tired im not doing a merge for this

--- src/routes/Usuario.routes.ts
+++ src/routes/Usuario.routes.ts
@@ -1,11 +1,11 @@
 import {Router} from 'express'
 import { createUser, deleteUser, getUser, updateUser } from '../controllers/usuario.controllers'
 import { getUsers } from '../controllers/usuario.controllers'
-
+import {verifyToken} from '../middleware/auth.middleware'
 const router = Router()
 
-router.post('/usuario', createUser)
-router.get('/usuario', getUsers)
+router.post('/usuario', verifyToken, createUser)
+router.get('/usuario', verifyToken, getUsers)
 router.put('/usuario/:id',verifyToken, updateUser)
 router.delete('/usuario/:id',verifyToken, deleteUser)
 router.get('/usuario/:id', verifyToken, getUser)
