4846cf67c64335e3ed8b125c231a61533a6a5ac0
JosephByronLane <2419141@modelo.edu.mx>
2023-12-07 23:59:56 -0600

    forgot to change back to remote db

--- src/db.ts
+++ src/db.ts
@@ -8,7 +8,7 @@ import {ProductosEnPromocion} from './models/ProductosEnPromocion'
 
 export const AppDataSource= new DataSource({
     type: 'mysql',
-    host:  'localhost',
+    host:  '34.234.63.150',
     username: 'root',
     password:'Adminadmin1!',
     database: 'api_equipo2',
