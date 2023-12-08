fe2fc5be35846ff3c9004486fdc4c80cab8057df
JosephByronLane <2419141@modelo.edu.mx>
2023-12-08 00:05:09 -0600

    fixed error when compilinc tsc

--- src/middleware/rate-limiter.middleware.ts
+++ src/middleware/rate-limiter.middleware.ts
@@ -1,5 +1,6 @@
 const { RateLimiterMemory } = require('rate-limiter-flexible');
 import { Request,Response, NextFunction } from "express";
+import { RateLimiterRes } from "rate-limiter-flexible";
 const shortTermLimiterFlex = new RateLimiterMemory({
     points: 3, // Number of requests
     duration: 5, // Per seconds seconds
@@ -16,7 +17,8 @@ export async function rateLimitMiddleware(req: Request, res: Response, next: Nex
 
         next(); 
     } catch (rejRes) {
-        const timeToWait = rejRes.msBeforeNext / 1000;
+        const rateLimiterResponse = rejRes as RateLimiterRes;
+        const timeToWait = rateLimiterResponse.msBeforeNext / 1000;
         const remainingTime = timeToWait > 60 ? `${Math.round(timeToWait / 60)} minutes` : `${timeToWait} seconds`;
         res.status(429).send({message: `Too many requests. Try again in ${remainingTime}. `});
     }
